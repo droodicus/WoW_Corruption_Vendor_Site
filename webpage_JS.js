1da2a8bc28a3b5efdc03fcb9862180ef5bf76ca9
Karl Olsen <droodicus@gmail.com>
2020-07-15 17:48:06 -0400

    Initial commit

---
+++ webpage_JS.js
@@ -0,0 +1,13 @@
+class Rotation	{
+	constructor(la, na, nc, c1, c2, c3, c4, c5, c6, c7)
+	this.lastAvailable = la;
+	this.nextAvailable = na;
+	this.numCorruptions = nc;
+	this.c1 = c1;
+	this.c2 = c2;
+	this.c3 = c3;
+	this.c4 = c4;
+	this.c5 = c5;
+	this.c6 = c6;
+	this.c7 = c7;
+}
